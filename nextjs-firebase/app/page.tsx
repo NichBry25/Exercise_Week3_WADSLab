57f2a8c3138504166843cebeeff216abe02ec33b
NichBry25 <nicholas.bryan001@binus.ac.id>
2026-02-19 11:55:07 +0700

    test auth

---
+++ nextjs-firebase/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home(){
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <Button variant="outline">Button</Button>
+    </div>
+  )
+}
